4c7a0c0503d529e8e2faf22b87baa15706055167
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-10-15 01:26:50 +0700

    feat: cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import './Cart.css'
-import { food_list } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
@@ -10,7 +9,7 @@ const Cart = () => {
     return (
         <div className='cart'>
             <div className='cart-items'>
-                <div className='cartt-items-title'>
+                <div className='cart-items-title'>
                     <p>Items</p>
                     <p>Title</p>
                     <p>Price</p>
@@ -24,18 +23,53 @@ const Cart = () => {
                     food_list.map((item, index) => {
                         if (cartItems[item._id] > 0) {
                             return (
-                                <div key={index} className='cart-items-title cart-items-item'>
-                                    <img src={item.image} alt={item.name} />
-                                    <p>{item.name}</p>
-                                    <p>{item.price}</p>
-                                    <p>{cartItems[item._id]}</p>
-                                    <p>{Number(item.price) * Number(cartItems[item._id])}</p>
-                                    <p>x</p>
+                                <div>
+                                    <div key={index} className='cart-items-title cart-items-item'>
+                                        <img src={item.image} alt={item.name} />
+                                        <p>{item.name}</p>
+                                        <p>${item.price}</p>
+                                        <p>{cartItems[item._id]}</p>
+                                        <p>${Number(item.price) * Number(cartItems[item._id])}</p>
+                                        <p style={{ cursor: 'pointer' }} onClick={() => removeFromCart(item._id)}>x</p>
+                                    </div>
+                                    <hr />
                                 </div>
                             )
                         }
                     })
                 }
+            </div>
+
+            <div className='cart-bottom'>
+                <div className='cart-total'>
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className='cart-total-details'>
+                            <p>Subtotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className='cart-total-details'>
+                            <p>Delivery Fee</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className='cart-total-details'>
+                            <b>Total</b>
+                            <b>{0}</b>
+                        </div>
+                    </div>
+                    <button>PROCESS TO CHECKOUT</button>
+                </div>
+                <div className='cart-promocode'>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, veritatis?</p>
+                        <div className='cart-promocode-input'>
+                            <input type="text" placeholder='Promo code' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
