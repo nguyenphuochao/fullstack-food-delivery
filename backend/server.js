9c6be351df6999a91899fb7ab1982a969384d5de
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-10-19 01:13:40 +0700

    feat(api): login & register user

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from 'cors'
-import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
+import 'dotenv/config'
+import { connectDB } from './config/db.js'
+import foodRouter from './routes/foodRoute.js'
+import userRouter from './routes/userRoute.js'
 
 // app config
 const app = express();
@@ -17,6 +19,7 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
     res.send("API working");
