fc62d36e4e9a919dc001e14ccc1c28fab739bc47
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-10-18 15:24:02 +0700

    feat(backend): api endpoits foods

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
 
 // app config
 const app = express();
 const port = 4000;
 
-// db connection
-connectDB()
-
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-// defind API endpoint
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
 app.get("/", (req, res) => {
-    res.send("API working")
+    res.send("API working");
 })
 
 // start server with port
