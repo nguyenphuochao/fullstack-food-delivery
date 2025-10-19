0a289468a956729faf0d163b0604a80ffcc41aaf
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-10-19 16:25:49 +0700

    feat(admin): handle call api add food & api list food

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import Add from './pages/Add/Add'
@@ -6,16 +7,20 @@ import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
 
 const App = () => {
+
+    const url = 'http://localhost:4000';
+
     return (
         <div>
+            <ToastContainer />
             <Navbar />
             <hr />
             <div className='app-content'>
                 <Sidebar />
                 <Routes>
-                    <Route path="/add" element={<Add />} />
-                    <Route path="/list" element={<List />} />
-                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/add" element={<Add url={url} />} />
+                    <Route path="/list" element={<List url={url} />} />
+                    <Route path="/orders" element={<Orders url={url} />} />
                 </Routes>
             </div>
         </div>
